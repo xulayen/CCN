a7f75b1641173ec8574dd96c3911d30353fd0f4c
xulayen <l>
2016-12-23 16:26:06 +0800

    insert

---
+++ CCN.YD/static/script/main.js
@@ -0,0 +1,15 @@
+﻿document.write('<script src="../../jquery/jquery-2.2.0.min.js" type="text/javascript"></script>');
+document.write('<script src="../../jquery/Plugins/jquery.base64.js" type="text/javascript"></script>');
+document.write('<script src="../../jquery/Plugins/jquery.form.red.js" type="text/javascript"></script>');
+document.write('<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript"></script>');
+document.write('<script src="../../jquery/Plugins/Jquery.WeChat.1.2.0.js" type="text/javascript"></script>');
+document.write('<script src="../../layer/layer.js" type="text/javascript"></script>');
+document.write('<script src="../../bootstrap-3.3.6-dist/js/bootstrap.js" type="text/javascript"></script>');
+document.write('<script src="../../angular/angular.js" type="text/javascript"></script>');
+document.write('<script src="../../angular/angular-resource.js" type="text/javascript"></script>');
+document.write('<script src="../../angular/angular-route.min.js" type="text/javascript"></script>');
+document.write('<script src="../../script/function.js" type="text/javascript"></script>');
+document.write('<script src="../services/services.js" type="text/javascript"></script>');
+document.write('<script src="../directives/directives.js" type="text/javascript"></script>');
+document.write('<script src="../controller/main.js" type="text/javascript"></script>');
+    
